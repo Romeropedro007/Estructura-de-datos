4267b43d3c99844548370b19a18aa40ef65bc19a
Romeropedro007 <po.romeroc@uea.edu.ec>
2025-06-20 23:07:40 -0500

    Ingreso de datos

--- Experimental 1/Program.cs
+++ Experimental 1/Program.cs
@@ -8,4 +8,21 @@ class Turno
 
 // Esta clase maneja la lógica de la agenda usando un vector
 class AgendaTurnos
+{
+    Turno[] turnos = new Turno`[100];
+    // Contador para saber cuántos turnos hemos registrado
+    int contador = 0;
 
+    // Método para agendar un nuevo turno
+    public void AgendaTurnos()
+    {
+        //Validamos si aún hay espacio en el vector
+        if (contador < turnos.Length)
+        {
+            //Nuevo objeto tipo turno
+            Turno nuevo = new Turno();
+
+            //Solicitamos ingreso de datos del paciente
+            
+        }
+    }
