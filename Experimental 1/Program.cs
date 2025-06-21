bd0cc0f8bedaf18d8312bc7b813135fd17ccfd9c
Romeropedro007 <po.romeroc@uea.edu.ec>
2025-06-20 22:26:30 -0500

    Definicion de estructura

--- Experimental 1/Program.cs
+++ Experimental 1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Aquí definimos la estructura del turno como si fuera un registro
+class Turno
+{
+    public string nombre;   // Nombre del paciente    public string cedula;   // Cédula del paciente
+    public string fecha;    // Fecha del turno
+    public string hora;     // Hora del turno
+    }
+
+// Esta clase maneja la lógica de la agenda usando un vector
+class AgendaTurnos
+
