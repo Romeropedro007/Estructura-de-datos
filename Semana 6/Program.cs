09f55f82b8d5f5e2caa2b9a0e891c4e661382474
Romeropedro007 <po.romeroc@uea.edu.ec>
2025-07-06 20:07:27 -0500

    numeros i

--- Semana 6/Program.cs
+++ Semana 6/Program.cs
@@ -2,3 +2,12 @@
 {
     static void Main(string[] args)
     {
+        int[] numeros = new int[5];
+        int suma = 0;
+
+        for (int i = 0; i < 5; i++)
+        {
+            System.Console.Write("Ingrese un número: ");
+            numeros[i] = int.Parse(System.Console.ReadLine());
+            suma += numeros[i];
+        }
